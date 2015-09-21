e39edebfe8e11309ec483518e28121adf527c43a
shamtickler <nathanisonline@gmail.com>
2015-09-20 21:29:09 -0400

    Initial Commit

---
+++ Car Test Project/Assets/Scripts/Car.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+var frCollider : WheelCollider;
+var flCollider : WheelCollider;
+var rrCollider : WheelCollider;
+var rlCollider : WheelCollider;
+
+var frTireCollider : Transform;
+var flTireCollider : Transform;
+var rrTireCollider : Transform;
+var rlTireCollider : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+var pos : Vector3;
+var quat : Quaternion;
+frCollider.GetWorldPose(pos,quat);
+frTireCollider.position = pos;
+frTireCollider.rotation = quat;
+flCollider.GetWorldPose(pos,quat);
+flTireCollider.position = pos;
+flTireCollider.rotation = quat;
+rlCollider.GetWorldPose(pos,quat);
+rlTireCollider.position = pos;
+rlTireCollider.rotation = quat;
+rrCollider.GetWorldPose(pos,quat);
+rrTireCollider.position = pos;
+rrTireCollider.rotation = quat;
+}
