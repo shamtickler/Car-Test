135e87d36723db5bcc51cda76b473d6617a88554
shamtickler <nathanisonline@gmail.com>
2015-09-22 21:06:17 -0400

    Turned into particle testing ground

---
+++ Car Test Project/Assets/Scripts/MoveForward.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+var speed : float;
+function Start () {
+
+}
+
+function Update () {
+transform.Translate(Vector3.forward * speed * Time.deltaTime);
+}
---
+++ Car Test Project/Assets/Scripts/ParticleTester.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var particle : GameObject;
+var layerMask : LayerMask;
+var hit : RaycastHit;
+var spawnPosition : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+
+if (Input.GetButtonDown("Fire1")){
+
+var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+ 
+if (Physics.Raycast (ray, hit, 1000)) {
+
+var shootDirection : Vector3;
+shootDirection.y = spawnPosition.position.y;
+shootDirection.x=hit.point.x;
+shootDirection.z=hit.point.z;
+
+var spawnedEffect : GameObject = Instantiate(particle, spawnPosition.position, transform.rotation);
+spawnedEffect.transform.LookAt(shootDirection, Vector3.up);
+
+} 
+
+
+
+
+
+}
+
+
+
+}
