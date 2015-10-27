edde73f9cdead39f737ef8f7892890ef674a9bb1
shamtickler <nathanisonline@gmail.com>
2015-10-26 22:40:25 -0400

    ScrollingWorks
    kk blowin swag nuts

---
+++ Car Test Project/Assets/Scripts/DropGame/MenuExclusive/RotateStacker.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateStacker : MonoBehaviour {
+
+    [SerializeField]
+    private float rotateSpeed = 1;
+
+
+    public float rotAngle = 360;
+
+    private float currentRotation = 0;
+    private bool rotateMe = false;
+
+
+    void Start()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (rotateMe)
+        {
+
+            transform.rotation = Quaternion.Euler(currentRotation,currentRotation,currentRotation);
+            currentRotation += (Time.deltaTime * rotateSpeed);
+            if (currentRotation >= rotAngle)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                currentRotation = 0;
+                rotateMe = false;
+            }
+
+        }
+
+    }
+
+    public void RotateButton()
+    {
+        rotateMe = true;
+    }
+}
---
+++ Car Test Project/Assets/Scripts/DropGame/MenuExclusive/StackJump.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StackJump : MonoBehaviour {
+
+    [SerializeField]
+    private float multiplier = 2;
+
+    private bool isJumping = false;
+    private Vector3 movement = Vector3.zero;
+    private float x = 0;
+    private Vector3 startingPosition = Vector3.zero;
+
+
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isJumping)
+        {
+            movement.y = Mathf.Sin((x * multiplier * Mathf.PI));
+            transform.position += movement;
+            x += Time.deltaTime;
+            if (x >= 0.5f)
+            {
+                isJumping = false;
+                movement.y = 0;
+                transform.position = startingPosition;
+            }
+
+        }
+        
+	}
+
+    public void JumpTheCube()
+    {
+        isJumping = true;
+        x = 0;
+    }
+
+}
